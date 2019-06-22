fac33a01e4c71fa89a3cd022b13593322bb79860
Ben Thow <bthow@mkmhealth.com>
2019-06-23 00:46:59 +1000

    Form submission using reactjs

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,30 +1,48 @@
 import React from "react"
-import addToMailchimp from 'gatsby-plugin-mailchimp'
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
 class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
+    })
+      .then(() => alert('Thanks, ' + form.getAttribute("action") + 'Sent'))
+      .catch(error => alert(error));
+  };
 
   render() {
      return (
-      <form name="contact" method="POST" data-netlify="true">
-	Your thoughts
-	<input
-	    type="text"
-		    placeholder="name"
-		    name="name"
-		/>
-		<input
-		    type="email"
-		    placeholder="email"
-		    name="email"
-		/>
-		<input
-		    type="comment"
-		    placeholder="comment"
-		    name="comment"
-		/>
-		<input type="submit" />
-      </form>
-	
+	<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit}>
+	Your thoughts - Emails aren't published
+	  <input type="hidden" name="form-name" value="contact" />
+	  <input type="text" name="name" placeholder="Name"  onChange={this.handleChange}/>
+	  <input type="email" name="email" placeholder="email"  onChange={this.handleChange} />
+	  <textarea name="message" placeholder="Message" onChange={this.handleChange} ></textarea>
+<p>
+            <button type="submit">Send</button>
+</p>
+	</form>
     )
   }
 }
